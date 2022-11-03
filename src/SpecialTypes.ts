fadf9d6992cfebd15dd09d9426c80e13575515aa
Sungsu <tjdtn951007@gmail.com>
2022-11-03 22:52:40 +0900

    Typescript SpecialTypes

---
+++ src/SpecialTypes.ts
@@ -0,0 +1,68 @@
+// Union Type
+let user :(number | string) = 'someone';
+console.log(user, typeof user);
+user = 123;
+console.log(user, typeof user);
+
+let usersArr :(string | number)[]
+    = ['PersonA', 'PersonB', 'PersonC', 123];
+
+let usersObj :{name: string, age: number, description: number|string}
+    = {name: 'PersonA', age: 28, description: 123};
+
+// Any Type
+let anything: any;
+anything = 123;
+console.log(anything, typeof anything);
+anything = 'hihihi';
+console.log(anything, typeof anything);
+anything = [];
+console.log(anything, typeof anything);
+
+// Unknown Type
+let myUnknown: unknown;
+myUnknown = 123;
+console.log(myUnknown, typeof myUnknown);
+myUnknown = 'mimimi';
+console.log(myUnknown, typeof myUnknown);
+myUnknown = [];
+console.log(myUnknown, typeof myUnknown);
+
+// Any vs Unknown
+let anythingTest: any;
+let someVar1: string = anythingTest;
+let someVar2: boolean = anythingTest;
+let someVar3: number = anythingTest;
+
+let unknownTest: unknown;
+// let someVar4: string = unknownTest;
+// let someVar5: boolean = unknownTest;
+// let someVar6: number = unknownTest;
+
+let someoneName: unknown;
+// someoneName[0];
+// someoneName-1;
+// someoneName.data;
+
+
+
+
+// homework 1
+let userName: string = 'kim';
+let userAge: undefined|number = undefined;
+let married: boolean = false;
+let Lee: (string | undefined | number | boolean)[] = [userName, userAge, married];
+
+// homework 2
+type schoolProperty = {
+    score: (number|boolean)[],
+    teacher: string,
+    friend: string | string[]
+}
+let school: schoolProperty = {
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
+}
+school.score[4] = false;
+school.friend = ['Lee' , school.teacher]
