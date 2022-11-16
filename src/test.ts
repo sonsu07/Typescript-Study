218f153d639bcac8e8837093dd0dd59918d5103d
sonsu <sonsuwdev@gmail.com>
2022-11-16 10:36:29 +0900

    package.json modify

--- src/test.ts
+++ src/test.ts
@@ -1,39 +1,9 @@
+let i: number;
 
-
-
-/**
- * index signature
- */
-interface someOneInterface {
-    [index: string] : string | number
-    name: string;
-    age: number;
-    born: string;
+for (i = 0; i < 5; i++) {
+    console.log('a');
+    setTimeout(function() {
+        console.log(i);
+    }, 100);
+    console.log('b');
 }
-const someOne: someOneInterface = {
-    name: 'Kim',
-    age: 21,
-    born: 'Busan'
-}
-for (let prop in someOne) {
-    console.log(`${prop} ${someOne[prop]}`);
-}
-
-class Person {
-    [index: string] : string | number
-    name: string;
-    age: number;
-    born: string;
-    constructor(name: string, age: number, born: string) {
-        this.name = name;
-        this.age = age;
-        this.born = born;
-    }
-}
-
-const lee = new Person('Lee', 28, 'Busan');
-for (let prop in lee) {
-    console.log(`${prop} ${lee[prop]}`);
-}
-
-export {}
