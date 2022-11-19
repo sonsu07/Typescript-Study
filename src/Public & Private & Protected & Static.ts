77c87166916d645d4e870c912e87141c86e5a56e
sonsu0710 <sonsu0710@gmail.com>
2022-11-19 23:40:51 +0900

    Public & Private 개념 private로 선언된 변수 수정은 prototype 함수 생성을 통해서 해주도록...

--- src/Public & Private & Protected & Static.ts
+++ src/Public & Private & Protected & Static.ts
@@ -1,7 +1,6 @@
 // Public
 class PublicUser {
     public name: string;
-
     constructor(){
         this.name = 'kim';
     }
@@ -15,7 +14,6 @@ publicUser1.name = 'park';  //가능
 class PrivateUser {
     public name :string;
     private familyName :string;
-
     constructor(){
         this.name = 'Sungsu';
         this.familyName = 'Lee'; //가능
@@ -25,5 +23,25 @@ class PrivateUser {
 let privateUser1 = new PrivateUser();
 privateUser1.name = 'park';  //가능
 // privateUser1.familyName = 456; //에러남
+
+
+/**
+ * Q1. private 부여된 속성을 class 밖에서 수정해야할 경우?
+ */
+class PrivateUserExchangeExample {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    changeName(change: string) {
+        this.name = change;
+    }
+}
+
+let privateUser = new PrivateUserExchangeExample('Sungsu');
+console.log(privateUser);
+privateUser.changeName('SunAh');
+console.log(privateUser);
+
 
 export {}
