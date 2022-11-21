96ed4b377354f390fb7a96e6aef5db0e453e91b0
sonsu0710 <sonsu0710@gmail.com>
2022-11-21 20:35:04 +0900

    Public & Private & Protected & static 개념

--- src/Public & Private & Protected & Static.ts
+++ src/Public & Private & Protected & Static.ts
@@ -1,4 +1,6 @@
 // Public
+import test from "node:test";
+
 class PublicUser {
     public name: string;
     constructor(){
@@ -21,8 +23,12 @@ class PrivateUser {
 }
 
 let privateUser1 = new PrivateUser();
-privateUser1.name = 'park';  //가능
+privateUser1.name = 'park';  // private 변수는 수정 금지
 // privateUser1.familyName = 456; //에러남
+// console.log(privateUser1.familyName) // 점 표기법으로는 private 변수를 불러와 사용 못함
+console.log(privateUser1['name']); // 괄호 표기법으로는 private 변수에 접근 가능.
+privateUser1['name'] = 'kang'; // 수정하는 것도 가능하다.
+console.log(privateUser1['name']);
 
 
 /**
@@ -43,5 +49,105 @@ console.log(privateUser);
 privateUser.changeName('SunAh');
 console.log(privateUser);
 
+
+// Protected
+class MyUser {
+    private privateVal;
+    protected protectedVal;
+   constructor() {
+       this.privateVal = 'private 변수입니다';
+       this.protectedVal = 'protected 변수입니다';
+   }
+}
+
+class ProtectUser extends MyUser{
+    doExtends() {
+        this.protectedVal = 'extends를 통해 상속받은' + this.protectedVal;
+        console.log(this.protectedVal);
+    }
+    constructor() {
+        super();
+        this.protectedVal = 'extends를 통해 상속받은 클래스에서 constructor를 사용한 ' + this.protectedVal;
+    }
+}
+const myUser = new MyUser();
+console.log(myUser);
+// console.log(myUser.privateVal); // private 변수에 접근 불가
+// console.log(myUser.protectedVal); // protected 변수에 접근 불가
+
+const protectedUser = new ProtectUser()
+console.log(protectedUser);
+// console.log(protectedUser.privateVal); // private 변수에 접근 불가
+// console.log(protectedUser.protectedVal); // protected 변수에 접근 불가
+
+protectedUser.doExtends(); // protectedVal 변경
+console.log(protectedUser['protectedVal']); // 괄호 표기법으로 확인
+const protectedUser2 = new ProtectUser();
+console.log(protectedUser2);
+
+// Static
+class StaticUser {
+    static skill = 'JS';
+    intro = StaticUser.skill + ' 전문가';
+}
+let staticUser1 = new StaticUser();
+console.log(staticUser1.intro);
+console.log(StaticUser);
+
+StaticUser.skill = 'TS';
+let staticUser2 = new StaticUser();
+console.log(staticUser2.intro);
+
+
+/**
+ * Q2. 다음 x, y, z 속성의 특징을 설명해보십시오.
+ */
+class User {
+    private static x = 10;
+    public static y = 20;
+    protected z = 30;
+    modifyX() {
+        User.x = 20;
+    }
+    constructor() {
+        this.z = 40;
+    }
+}
+
+// console.log(User);
+// const testUser = new User();
+// testUser.modifyX();
+// console.log(User);
+
+// User.y = 30;
+// console.log(User);
+
+class ExtendsUser extends User {
+    some = this.z;
+    constructor() {
+        super();
+        this.z = 40;
+    }
+}
+
+/**
+ * Q3. x 속성에 숫자를 더해주는 함수
+ */
+
+class Q3User {
+    private static x = 10;
+    public static y = 20;
+
+    static addOne(a: number) {
+        return Q3User.x + a;
+    }
+
+    static printX() {
+        console.log(Q3User.x);
+    }
+}
+Q3User.addOne(3) //이렇게 하면 x가 3 더해져야함
+Q3User.addOne(4) //이렇게 하면 x가 4 더해져야함
+Q3User.printX()  //이렇게 하면 콘솔창에 x값이 출력되어야함
 
 export {}
